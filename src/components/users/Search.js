e01fb6c027e29fec3fe1cfea7f496a6096083b97
Hunaid <61511895+Hunaid-Memon@users.noreply.github.com>
2021-10-18 00:35:59 +0500

    Update Search Component
    Update search component from class to function using useState method

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,62 +1,79 @@
 import React from 'react'
-import { Component } from 'react'
+import { useState } from 'react'
 import PropTypes from 'prop-types'
 
+const Search = ({ showClear, clearUsers, setAlert, searchUsers }) => {
 
-class Search extends Component{
+    const [text, setText] = useState('');
 
-    state = {
-        text: ''
+
+
+        // class base component 
+// class Search extends Component{
+//     state = {
+//         text: ''
+    // }
+
+            // use proptype in class component
+    // static propTypes = {
+    //     searchUsers: PropTypes.func.isRequired,
+    //     clearUsers: PropTypes.func.isRequired,
+    //     showClear: PropTypes.bool.isRequired,
+    //     setAlert: PropTypes.func.isRequired,
+    // }
+
+    // this method used for search and class base
+    // onChange = (e) => {
+            // Update text filled directly
+        // this.setState( {text: e.target.value} )
+             // Update text file dynamically
+        // this.setState({ [e.target.name ]: e.target.value })
+    // }
+            // Convert in function base using useState
+    const onChange = e => setText( e.target.value )
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+                // if (this.state.text === '') {
+        if (text === '') {
+            setAlert('Please Enter a username', 'light');
+        } else {
+            // Sending data to App component
+            searchUsers(text);
+            setText('');
+        }
     }
 
-    static propTypes = {
+        //use in class component
+    // render(){
+                // Destructuring in class 
+        // const {showClear, clearUsers} = this.props;
+
+    return (
+        <div>
+            <form onSubmit={onSubmit} className="form">
+                <input type="text" name='text' 
+                value={text} placeholder='Search Users' 
+                onChange={onChange}
+                />
+                <input type="submit" value="Search" className='btn btn-dark btn-block' />
+            </form>
+                    {/* before destructring */}
+            {/* {this.props.showClear && */}
+            {showClear &&
+                // <button onClick={this.props.clearUsers} className="btn btn-light btn-block">Clear</button>
+                <button onClick={clearUsers} className="btn btn-light btn-block">Clear</button>
+            }
+            </div>
+    )
+}
+// }
+
+ Search.prototype = {
         searchUsers: PropTypes.func.isRequired,
         clearUsers: PropTypes.func.isRequired,
         showClear: PropTypes.bool.isRequired,
         setAlert: PropTypes.func.isRequired,
     }
 
-    // this method used for search
-    onChange = (e) => {
-        // Update text filled directlt
-        // this.setState( {text: e.target.value} )
-        // Update text file dynamically
-        this.setState({ [e.target.name ]: e.target.value })
-    }
-
-    onSubmit = (e) => {
-        e.preventDefault();
-        if (this.state.text === '') {
-            this.props.setAlert('Please Enter a username', 'light');
-        } else {
-            // Sending data to App component
-            this.props.searchUsers(this.state.text);
-            this.setState({ text: '' })
-        }
-    }
-
-    render(){
-                // Destructuring
-        const {showClear, clearUsers} = this.props;
-
-        return (
-            <div>
-                <form onSubmit={this.onSubmit} className="form">
-                    <input type="text" name='text' 
-                    value={this.state.text} placeholder='Search Users' 
-                    onChange={this.onChange}
-                    />
-                    <input type="submit" value="Search" className='btn btn-dark btn-block' />
-                </form>
-                        {/* before destructring */}
-                {/* {this.props.showClear && */}
-                {showClear &&
-                    // <button onClick={this.props.clearUsers} className="btn btn-light btn-block">Clear</button>
-                    <button onClick={clearUsers} className="btn btn-light btn-block">Clear</button>
-                }
-             </div>
-        )
-    }
-}
-
-export default Search
+export default Search;
